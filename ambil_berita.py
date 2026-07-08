d738e84b94dece83269ef70cdc789a89e53db2c1
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-07-08 18:52:00 +0700

    Update ambil_berita.py

--- ambil_berita.py
+++ ambil_berita.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 BASE_URL = "https://pulnew.pages.dev"
 sumber_rss = [
     {"media": "Antara", "url": "https://www.antaranews.com/rss/nasional"},
-    {"media": "Pikiran Rakyat", "url": "https://www.pikiran-rakyat.com/feed"}
+    {"media": "Pikiran Rakyat", "url": "https://www.pikiran-rakyat.com/feed"},
     {"media": "Tribunnews", "url": "https://www.tribunnews.com/rss"},
     {"media": "Republika", "url": "https://www.republika.co.id/rss"},
     {"media": "Okezone", "url": "https://sindonews.com/rss"},
