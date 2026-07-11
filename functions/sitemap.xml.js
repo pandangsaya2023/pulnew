8ed9e6144861de435a545bc73031aa89c1d826d2
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-07-11 13:38:28 +0700

    Refactor sitemap generation with API integration
    Updated sitemap generation to fetch post URLs from API and improved XML response headers.

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -1,24 +1,33 @@
 export async function onRequest(context) {
   const baseUrl = 'https://pulnew.pages.dev';
-  const { env } = context;
 
-  // Ambil list file dari folder /public/posts/
-  const postsDir = await env.ASSETS.fetch(new Request(baseUrl + '/posts/'));
-  const files = await postsDir.text();
+  // 1. Panggil API posts.js yg udah ada
+  const res = await fetch(new URL('/api/posts', baseUrl));
+  const downloadUrls = await res.json(); // isinya array url
 
-  // Ambil semua nama file.json dari hasil HTML folder
-  const slugs = [...files.matchAll(/href="(.+?)\.json"/g)].map(m => m[1]);
+  // 2. Ubah jadi slug
+  const posts = downloadUrls.map(url => {
+    const slug = url.split('/').pop().replace('.json', '');
+    return { slug };
+  });
 
+  // 3. Buat XML
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>${baseUrl}/</loc><priority>1.0</priority></url>
-  ${slugs.map(slug => `
+  ${posts.map(p => `
   <url>
-    <loc>${baseUrl}/berita/${slug}</loc>
+    <loc>${baseUrl}/berita/${p.slug}</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>`).join('')}
 </urlset>`;
 
-  return new Response(xml, { headers: { 'Content-Type': 'application/xml' } });
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600'
+    }
+  });
 }
