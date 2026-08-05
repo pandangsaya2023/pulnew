a929e30d603483e112f01a55dc120400db86daf7
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-08-05 10:22:33 +0700

    Add new RSS sources for news retrieval

--- ambil_berita.py
+++ ambil_berita.py
@@ -19,7 +19,10 @@ MAX_BERITA_PER_RUN = 15
 
 sumber_rss = [
     {"media": "Kompas", "url": "https://indeks.kompas.com/nasional"},
-    {"media": "Republika", "url": "https://www.republika.co.id/rss"},
+    {"media": "Antara", "url": "https://www.antaranews.com/rss/tekno"},
+    {"media": "Antara"", "url": "https://www.antaranews.com/nasional"},
+    {"media": "Okezone", "url": "https://sindonews.com/rss"},
+    {"media": "Republika", "url": "https://www.republika.co.id/rss"}
 ]
 
 # --- FUNGSI: BIKIN NAMA MEDIA DARI URL ---
