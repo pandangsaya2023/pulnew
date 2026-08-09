420c0ead66a6ff18e470eeab66cdcb4713ee8d35
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-08-09 19:23:52 +0700

    Uncomment Antara RSS source in ambil_berita.py

--- ambil_berita.py
+++ ambil_berita.py
@@ -19,7 +19,7 @@ MAX_BERITA_PER_RUN = 15
 
 sumber_rss = [
     {"media": "Kompas", "url": "https://indeks.kompas.com/nasional"},
-    # {"media": "Antara", "url": "https://www.antaranews.com/rss/tekno"},
+    {"media": "Antara", "url": "https://www.antaranews.com/rss/tekno"},
     {"media": "Antara", "url": "https://www.antaranews.com/nasional"},
     # {"media": "Okezone", "url": "https://sindonews.com/rss"},
     {"media": "Republika", "url": "https://www.republika.co.id/rss"}
