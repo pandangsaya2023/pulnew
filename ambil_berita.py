a3561f7ab5250cd7bc420d2514a2a39cede9e755
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-08-18 18:16:30 +0700

    Update ambil_berita.py

--- ambil_berita.py
+++ ambil_berita.py
@@ -20,7 +20,8 @@ MAX_BERITA_PER_RUN = 15
 
 sumber_rss = [
     {"media": "Kompas", "url": "https://indeks.kompas.com/nasional"},
-    {"media": "Antara", "url": "https://www.antaranews.com/nasional"},
+    {"media": "Antara", "url": "https://www.antaranews.com/rss/tekno"},
+    # {"media": "Antara", "url": "https://www.antaranews.com/nasional"},
     {"media": "Republika", "url": "https://www.republika.co.id/rss"}
 ]
 
