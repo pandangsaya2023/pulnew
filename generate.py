22a4cb801bab996d75a81571262a533225741497
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-08-23 18:43:13 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -52,7 +52,7 @@ def generate_article_page(article):
     desc = (article.get('body', '')).replace('\n',' ')[:160] + "..."
     
     #img_url = f"{BASE_URL}{article.get('image','')}" # PENTING: JADI URL FULL
-    #page_url = f"{BASE_URL}/berita/{article['slug']}.html" # PENTING: URL FULL
+    page_url = f"{BASE_URL}/berita/{article['slug']}.html" # PENTING: URL FULL
 
     image_path = article.get('image', '') or article.get('thumbnail', '') or '/media/og-default.png'
     img_url = f"{BASE_URL}{image_path}" # PENTING: JADI URL FULL
