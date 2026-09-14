42f81e23aa8749dfa6fb08fe1da9e7328cd2a2c9
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-09-15 01:47:01 +0700

    Update description extraction in _worker.js
    Refactor description extraction logic to prioritize meta_description and adjust substring length.

--- public/_worker.js
+++ public/_worker.js
@@ -15,7 +15,8 @@ export default {
           
           const title = post.title ? `${post.title} - PULNEW` : "PULNEW";
           const rawBody = post.body || post.content || "";
-          const desc = rawBody.replace(/<[^>]*>/g, '').substring(0, 150) + "...";
+          //const desc = rawBody.replace(/<[^>]*>/g, '').substring(0, 150) + "...";
+          const desc = post.meta_description || (post.body || post.content || '').substring(0, 160).replace(/<[^>]*>/g, '') + '...';
           
           // --- PERBAIKAN OTOMATIS URL GAMBAR ---
           let rawImage = post.image || post.thumbnail || "/media/og-default.jpg";
