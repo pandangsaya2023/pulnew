569cecd3c8d56aeb41d01ccb76e23d67669ac87b
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-09-15 19:38:22 +0700

    Change favicon and apple-touch-icon file names
    Updated favicon and apple-touch-icon links in meta tags.

--- public/_worker.js
+++ public/_worker.js
@@ -35,8 +35,8 @@ export default {
           const metaTagsInjct = `
             <title>${title}</title>
             <meta name="description" content="${desc}" />
-            <link rel="icon" type="image/png" href="${url.origin}/logopulnew7.png" />
-            <link rel="apple-touch-icon" href="${url.origin}/logopulnew7.png" />
+            <link rel="icon" type="image/png" href="${url.origin}/logopulnew.png" />
+            <link rel="apple-touch-icon" href="${url.origin}/logopulnew.png" />
             <meta property="og:title" content="${post.title || 'PULNEW'}" />
             <meta property="og:description" content="${desc}" />
             <meta property="og:image" content="${image}" />
