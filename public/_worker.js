564a207f0036bfb7eaebbd0813c27be7ea8c8a62
pandangsaya2023 <pandangsaya2023@gmail.com>
2026-09-17 20:53:29 +0700

    Update _worker.js

--- public/_worker.js
+++ public/_worker.js
@@ -17,13 +17,9 @@ export default {
           // const title = post.title ? `${post.title} - PULNEW` : 'PULNEW';
 
           // SESUDAH (boleh beda):
-          // const judulSeo = post.seo_title || post.title;
-          // const title = judulSeo ? `${judulSeo} - PULNEW` : 'PULNEW';
-
-          const judulTampil = post.seo_title || post.title;
-          const title = judulTampil ? `${judulTampil} - PULNEW` : 'PULNEW';
-
-          
+          const judulSeo = post.seo_title || post.title;
+          const title = judulSeo ? `${judulSeo} - PULNEW` : 'PULNEW';
+  
           const rawBody = post.body || post.content || "";
           const cleanBody = rawBody.replace(/(<([^>]+)>)/ig, "");
           const desc = post.description || post.excerpt || (cleanBody ? cleanBody.substring(0, 150) + "..." : "");
